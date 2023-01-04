5821ddf468b1cd65bd26135db45f7cb2e43327f4
DaurenSan <sansizbai@gmail.com>
2023-01-04 12:40:08 +0600

    Second commit

--- src/main/java/com/sansizbai/Main.java
+++ src/main/java/com/sansizbai/Main.java
@@ -2,6 +2,6 @@ package com.sansizbai;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Dauren!");
+        System.out.println("Hello Vasya!");
     }
 }
