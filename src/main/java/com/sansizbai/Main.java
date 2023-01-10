440376e5994b42900977e2ce55c8500bff7a410e
DaurenSan <sansizbai@gmail.com>
2023-01-10 17:02:46 +0600

    connect to postgresql

--- src/main/java/com/sansizbai/Main.java
+++ src/main/java/com/sansizbai/Main.java
@@ -1,7 +1,23 @@
 package com.sansizbai;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Vasya!");
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/mydb", "postgres", "asdfg")) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
